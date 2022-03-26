2ee8e5cc5dcc6a82022522dd97e9e98ee80f24a1
Criss Guinther <crissrobertg@gmail.com>
2022-03-25 22:28:05 -0300

    Lista Dinamica

---
+++ CursoLogicaCapitulo12/src/Aluno.java
@@ -0,0 +1,22 @@
+
+public class Aluno {
+
+	String nome;
+
+//	public Aluno() {}
+//	
+//	public Aluno(String nome) {
+//		this.nome = nome;
+//	}
+//
+//	public String getNome() {
+//		return nome;
+//	}
+//
+//	public void setNome(String nome) {
+//		this.nome = nome;
+//	}
+
+	
+	
+}
